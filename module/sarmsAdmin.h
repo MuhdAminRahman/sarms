790a7ea510643e572a2042963d3b57bf2d06556b
MuhdAminRahman <lonemin1@gmail.com>
2024-11-28 07:03:49 +0800

    git commit

--- module/sarmsAdmin.h
+++ module/sarmsAdmin.h
@@ -111,6 +111,31 @@ void sarmsAdmin::registerAdmin()
     string username, password;
     string name, phoneno;
     string designation;
+    checkCin();
+
+    cout << "Username: ";           getline(cin,username);
+    while(dbA->checkUsername(username)){
+        cout << "\nUsername is already taken please insert a new one: ";
+        getline(cin,username);
+    }
+    cout << "\nPassword : ";       getline(cin,password);
+    cout << "\nFull name : "; getline(cin,name);
+    cout << "\nPhone number : ";    getline(cin,phoneno);
+
+    cout << "\nDesignation(Admin,Finance,Teacher) : ";     getline(cin,designation);
+    while (designation != "Admin" || designation != "Finance" || designation != "Teacher")
+    {
+        cout << "\nPlease type in the based on the example Admin,Finance,Teacher :";
+        getline(cin,designation);
+    }
+    
+    dbA->addUser(username, password, "Admin", name, phoneno, "NULL", "NULL", designation);
+}
+
+void sarmsAdmin::registerTeacher(){
+    string username, password;
+    string name, phoneno;
+    string designation;
     int choice;
     checkCin();
     
@@ -119,20 +144,45 @@ void sarmsAdmin::registerAdmin()
         cout << "\nUsername is already taken please insert a new one: ";
         getline(cin,username);
     }
-    cout << "\n Password : ";       getline(cin,password);
-    cout << "\nAdmin Full name : "; getline(cin,name);
+    cout << "\nPassword : ";        getline(cin,password);
+    cout << "\nFull name : ";       getline(cin,name);
     cout << "\nPhone number : ";    getline(cin,phoneno);
-    cout << "\nDesignation : ";     getline(cin,designation);
-    dbA->addUser(username, password, "Admin", name, phoneno, "NULL", "NULL", designation);
-}
-
-void sarmsAdmin::registerTeacher(){
-
+    dbA->addUser(username, password, "Teacher", name, phoneno, "NULL", "NULL", "Teacher");
 }
 
 void sarmsAdmin::registerParent(){
-
+    string username, password;
+    string name, phoneno;
+    string designation;
+    int choice;
+    checkCin();
+    
+    cout << "Username: ";           getline(cin,username);
+    while(dbA->checkUsername(username)){
+        cout << "\nUsername is already taken please insert a new one: ";
+        getline(cin,username);
+    }
+    cout << "\nPassword : ";        getline(cin,password);
+    cout << "\nFull name : ";       getline(cin,name);
+    cout << "\nPhone number : ";    getline(cin,phoneno);
+    cout << "\nDesignation : ";     getline(cin,designation);
+    dbA->addUser(username, password, "Parent", name, phoneno, "NULL", "NULL", designation);
 }
 void sarmsAdmin::registerStudent(){
-
+    string username, password;
+    string name, phoneno;
+    string designation;
+    int choice;
+    checkCin();
+    
+    cout << "Username: ";           getline(cin,username);
+    while(dbA->checkUsername(username)){
+        cout << "\nUsername is already taken please insert a new one: ";
+        getline(cin,username);
+    }
+    cout << "\nPassword : ";        getline(cin,password);
+    cout << "\nFull name : ";       getline(cin,name);
+    cout << "\nPhone number : ";    getline(cin,phoneno);
+    cout << "\nDesignation : ";     getline(cin,designation);
+    dbA->addUser(username, password, "Student", name, phoneno, "NULL", "NULL", designation);
 }
